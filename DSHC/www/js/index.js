2696399e47ba58133693a91c791339daef452f27
Vincent Pelletier <vincent_pelletier@trendmicro.com>
2016-07-11 21:23:50 -0400

    Everything.

--- DSHC/www/js/index.js
+++ DSHC/www/js/index.js
@@ -1,51 +1,56 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//Launch Help Center
+document.addEventListener("deviceready", onDeviceReady, false);
+var url = "https://help.deepsecurity.trendmicro.com/Welcome.html";
+//IAB reference
+var ref = null;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+$('.app').click(goToHelpCenter);
 
-        console.log('Received Event: ' + id);
-    }
-};
+function onDeviceReady() {
+	//override
+	window.open = cordova.InAppBrowser.open;
+	goToHelpCenter();
+	
+	
+}
 
-app.initialize();
+function goToHelpCenter() {
+	ref = cordova.InAppBrowser.open(url, '_blank', 'location=no');
+	ref.addEventListener('loadstart',iabLoadStart);
+	ref.addEventListener('loadstop', iabLoadStop);
+	ref.addEventListener('loaderror', iabLoadError);
+	ref.addEventListener('exit', iabClose);
+}
+
+function iabLoadStart(event) {
+	//alert('start: ' + event.url);
+}
+
+function iabLoadStop(event) {
+	//alert('stop: ' + event.url);
+	var js = "var menu = $('aside.right-off-canvas-menu');menu.addClass('left-off-canvas-menu');menu.removeClass('right-off-canvas-menu');var icon = $('.right-off-canvas-toggle');icon.addClass('left-off-canvas-toggle');icon.removeClass('right-off-canvas-toggle');icon.attr('style', 'right:" + (window.screen.width - 50) + "px;background-color:#333333;border-radius:4px;');var child = $(menu.children()[0]);child.attr('data-mc-css-sub-menu','left-submenu');";
+	js += "$(document).foundation('offcanvas', 'reflow');";
+	ref.executeScript({
+		code: js
+	}, function() {
+		//success
+	});
+	ref.insertCSS({
+		code: "div.link-list-popup{left:7px;margin-right:10px!important;overflow-y: scroll;max-height: 300px;}"
+	});
+}
+
+function iabLoadError(event) {
+	//alert('error: ' + event.message);
+	iabClose();
+}
+
+function iabClose(event) {
+	//alert('exit')
+	ref.removeEventListener('loadstart',iabLoadStart);
+	ref.removeEventListener('loadstop', iabLoadStop);
+	ref.removeEventListener('loaderror', iabLoadError);
+	ref.removeEventListener('exit', iabClose);
+	ref = null;
+	goToHelpCenter();
+}
